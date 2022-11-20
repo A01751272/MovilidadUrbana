1baff4dc8e512ae4d27b5431afc065d9bfcf73af
Pablo González de la Parra <a01745096@itesm.mx>
2022-11-20 17:33:02 -0600

    Crear documento de model

--- LogicaMultiagentes/model.py
+++ LogicaMultiagentes/model.py
@@ -1,40 +1,43 @@
 from mesa import Model
-from mesa.time import RandomActivation
+from mesa.time import StagedActivation
 from mesa.space import MultiGrid
 # from agent import *
 import json
 
 
-class CiudadModel(Model):
+class CityModel(Model):
     def __init__(self, N):
-        dataDictionary = json.load(open("mapDictionary.txt"))
+        map_dictionary = json.load(open("map_dictionary.txt"))
 
-        with open('base.txt') as baseFile:
-            lines = baseFile.readlines()
+        with open('map.txt') as map_file:
+            lines = map_file.readlines()
             self.width = len(lines[0])-1
             self.height = len(lines)
 
-            self.grid = MultiGrid(self.width, self.height,torus = False) 
-            self.schedule = RandomActivation(self)
+            self.grid = MultiGrid(self.width, self.height, torus=False)
+            self.schedule = StagedActivation(self)
 
             for r, row in enumerate(lines):
                 for c, col in enumerate(row):
-                    if col in ["v", "^", ">", "<"]:
-                        agent = Road(f"r{r*self.width+c}", self, dataDictionary[col])
+                    if col in ["v", "^", ">", "<", "x"]:
+                        agent = Road(f"r{r*self.width+c}", self,
+                                     map_dictionary[col])
                         self.grid.place_agent(agent, (c, self.height - r - 1))
-                    elif col in ["S", "s"]:
-                        agent = Traffic_Light(f"tl{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
+                    elif col in ["s"]:
+                        agent = Traffic_Light(f"tl{r*self.width+c}", self,
+                                              False if col == "S" else True,
+                                              int(map_dictionary[col]))
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.schedule.add(agent)
                     elif col == "#":
                         agent = Obstacle(f"ob{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
-                    elif col == "D":
+                    elif col == "e":
                         agent = Destination(f"d{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
         self.num_agents = N
-        self.running = True 
+        self.running = True
 
     def step(self):
         '''Advance the model by one step.'''
