0fa225f028c62aa9ae63c1357e9d9e133c4f7b14
A01752789 <69563637+A01752789@users.noreply.github.com>
2022-11-23 14:55:12 -0600

    Estacionamientos correctos

--- SimulacionMultiagentes/Assets/Scripts/MakeCity.cs
+++ SimulacionMultiagentes/Assets/Scripts/MakeCity.cs
@@ -13,10 +13,16 @@ public class MakeCity : MonoBehaviour
     [SerializeField] GameObject semaphorePrefab;
     [SerializeField] GameObject parkingPrefab;
     [SerializeField] int tileSize;
+
+    //Listas con coordenadas para prefabs especificos
     public List<(int,int)> upLight = new List<(int,int)> {(0,12), (1,12), (6,1), (7,1), (13,1), (14,1)};
     public List<(int,int)> downLight = new List<(int,int)> {(6,15), (7,15), (22, 6), (23, 6), (16,21), (17,21)};
     public List<(int,int)> leftLight = new List<(int,int)> {(5,-1), (5,0), (12,-1), (12,0), (21,8), (21,7)};
     public List<(int,int)> rightLight = new List<(int,int)> {(2,11), (2,10), (8,17), (8,16), (18,22), (18,23)};
+    public List<(int,int)> upParking = new List<(int,int)> {(10,6), (3,21)};
+    public List<(int,int)> downParking = new List<(int,int)> {(19,1), (3,18)};
+    public List<(int,int)> leftParking = new List<(int,int)> {(2,14), (18,13), (18,19)};
+    public List<(int,int)> rightParking = new List<(int,int)> {(5, 3), (12,3), (21,3), (5,14), (12,14), (12,19), (21,21)};
     // Start is called before the first frame update
     void Start()
     {
@@ -73,17 +79,27 @@ void MakeTiles(string tiles)
                 tile.transform.parent = transform;
                 x += 1;
             } else if (tiles[i] == 'e') {
-                position = new Vector3(x * tileSize, 0.5f, y * tileSize);
-                tile = Instantiate(parkingPrefab, position, parkingPrefab.transform.rotation);
-                tile.GetComponent<Renderer>().materials[0].color = Color.red;
+                position = new Vector3(x * tileSize, 0, y * tileSize);
+                tile = Instantiate(emptyPrefab, position, Quaternion.Euler(0, 0, 0));
+                tile.transform.parent = transform;
+                if (upParking.Contains((x, y))){
+                    tile = Instantiate(parkingPrefab, position, Quaternion.Euler(0, 180, 0));
+                } else if(downParking.Contains((x, y))){
+                    tile = Instantiate(parkingPrefab, position, Quaternion.Euler(0, 0, 0));
+                } else if(leftParking.Contains((x, y))){
+                    tile = Instantiate(parkingPrefab, position, Quaternion.Euler(0, 90, 0));
+                } else {
+                    tile = Instantiate(parkingPrefab, position, Quaternion.Euler(0, 270, 0));
+                }
                 tile.transform.parent = transform;
                 x += 1;
             } else if (tiles[i] == '#') {
                 position = new Vector3(x * tileSize, 0, y * tileSize);
                 tile = Instantiate(emptyPrefab, position, Quaternion.Euler(0, 0, 0));
                 tile.transform.parent = transform;
+                
                 tile = Instantiate(buildingPrefab, position, buildingPrefab.transform.rotation); //Quaternion.identity);
-                //tile.transform.localScale = new Vector3(0.3f, Random.Range(0.07f, 0.2f), 0.3f);
+                tile.transform.localScale = new Vector3(0.3f, Random.Range(0.07f, 0.2f), 0.3f);
                 tile.transform.parent = transform;
                 x += 1;
             } else if (tiles[i] == '\n') {
